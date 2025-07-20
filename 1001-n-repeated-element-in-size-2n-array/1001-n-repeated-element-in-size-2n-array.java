403da8c3c893f655fc12aed732538bec66053ba7
Sourabh kumar <161230977+Sourabhkumarcodes@users.noreply.github.com>
2025-07-20 19:02:26 +0530

    Time: 18 ms (14.76%), Space: 45.3 MB (46.81%) - LeetHub

--- 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/1001-n-repeated-element-in-size-2n-array.java
@@ -4,10 +4,10 @@ class Solution {
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
-        int mid = nums.length/2;
+        //int mid = nums.length/2;
         int val=0;
         for(int i:map.keySet()){
-            if(map.get(i)>=mid){
+            if(map.get(i)>=(nums.length/2)){
                 val= i;
             }
         }
