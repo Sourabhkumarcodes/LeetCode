0c22a8a4c364d71affc7d006fabbaea1671048f5
Sourabh kumar <161230977+Sourabhkumarcodes@users.noreply.github.com>
2025-07-23 20:34:28 +0530

    Time: 2 ms (36.93%), Space: 42.1 MB (24.59%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -3,15 +3,12 @@ class Solution {
         if(word==null||word.isEmpty()){
             return false;
         }
-        // if(word.length()==1){
-        //     return true;
-        // }
-        String str1=word.toUpperCase();
+        //String str1=word.toUpperCase();
         String str2=word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
-        String str3=word.toLowerCase();
-        if(word.equals(str1)){
+       // String str3=word.toLowerCase();
+        if(word.equals(word.toUpperCase())){
             return true;
-        }else if(word.equals(str3)){
+        }else if(word.equals(word.toLowerCase())){
             return true;
         }else if(word.equals(str2)){
             return true;
