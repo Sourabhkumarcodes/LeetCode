4ef543a41029363f258fc10fcab887cefbc7f273
Sourabh kumar <161230977+Sourabhkumarcodes@users.noreply.github.com>
2025-10-08 17:25:09 +0530

    Time: 11 ms (26.21%), Space: 45.7 MB (7.56%) - LeetHub

--- 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.java
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.java
@@ -5,7 +5,7 @@ class Solution {
             map.put(i,map.getOrDefault(i,0)+1);
         }
         int len=arr.length/4;
-        int cal=0;
+        //int cal=0;
         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
             if(entry.getValue()>len){
                 return entry.getKey();
