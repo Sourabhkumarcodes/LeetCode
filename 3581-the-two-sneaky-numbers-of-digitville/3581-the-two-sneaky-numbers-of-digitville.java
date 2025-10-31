90bd8bbb086752e3450054ee41c74bd291361fb3
Sourabh kumar <161230977+Sourabhkumarcodes@users.noreply.github.com>
2025-10-31 20:51:42 +0530

    Time: 2 ms (63.09%), Space: 46.7 MB (7.71%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -3,16 +3,16 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         int[] arr=new int[2];
         int pt=0;
-        int cnt=0;
+        //int cnt=0;
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
             if(map.get(i)==2){
                 arr[pt++]=i;
-                cnt++;
+                //cnt++;
             }
-            if(cnt==2){
-                break;
-            }
+            // if(cnt==2){
+            //     break;
+            // }
         }
      return arr;
     }
